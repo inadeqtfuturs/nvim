bfc522567662f1c59ad629b9b8b662c3b5cd3f04
inadeqtfuturs <alexj.christie@gmail.com>
2021-12-03 17:01:40 -0600

    [feat] update gitsigns which key. autocmd for highlight blame

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -8,6 +8,53 @@ function module.init(use)
 		},
 		config = function()
 			require("gitsigns").setup()
+
+			-- highlighting
+			vim.cmd([[
+        augroup custom_highlight
+          autocmd!
+          au ColorScheme * highlight GitSignsCurrentLineBlame guifg=#8389a3
+        augroup END
+      ]])
+
+			if WKOpts then
+				local wk = require("which-key")
+
+				local mappings = {
+					b = { "Blame line" },
+					p = { "Preview hunk" },
+					R = { "Reset buffer" },
+					r = { "Reset hunk" },
+					s = { "Stage hunk" },
+					S = { "Stage buffer" },
+					U = { "Reset buffer index" },
+					t = { "<cmd>Gitsigns toggle_current_line_blame<CR>", "Toggle blame" },
+					u = { "Undo stage hunk" },
+				}
+
+				local vmappings = {
+					s = { "Stage hunk" },
+					r = { "Reset hunk" },
+				}
+
+				wk.register(mappings, {
+					mode = "n",
+					prefix = "h",
+					buffer = nil,
+					silent = true,
+					noremap = true,
+					nowait = true,
+				})
+
+				wk.register(vmappings, {
+					mode = "v",
+					prefix = "h",
+					buffer = nil,
+					silent = true,
+					noremap = true,
+					nowait = true,
+				})
+			end
 		end,
 	})
 end

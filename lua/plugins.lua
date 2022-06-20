f18892976a46506a8890b8ec390cd1a9487c840e
inadeqtfuturs <alexj.christie@gmail.com>
2022-06-20 10:23:01 -0500

    feat(toggleterm): update to latest

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,51 +2,51 @@
 local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-	fn.system({ "git", "clone", "https://github.com/wbthomason/packer.nvim", install_path })
-	vim.cmd("packadd packer.nvim")
+  fn.system({ "git", "clone", "https://github.com/wbthomason/packer.nvim", install_path })
+  vim.cmd("packadd packer.nvim")
 end
 
 return require("packer").startup(function(use)
-	use({ "wbthomason/packer.nvim" })
+  use({ "wbthomason/packer.nvim" })
 
-	local configs = {
-		-- theme
-		"substrata",
+  local configs = {
+    -- theme
+    "substrata",
 
-		"which_key",
-		"dashboard",
-		"treesitter",
-		"telescope",
-		"nvimtree",
+    "which_key",
+    "dashboard",
+    "treesitter",
+    "telescope",
+    "nvimtree",
 
-		"cmp",
-		"lsp",
+    "cmp",
+    "lsp",
 
-		"barbar",
-		"indent_blankline",
-		"autopairs",
-		"autotag",
-		"surround",
-		"comments",
-		"cool",
-		"lightspeed",
-		"neogen",
+    "barbar",
+    "indent_blankline",
+    "autopairs",
+    "autotag",
+    "surround",
+    "comments",
+    "cool",
+    "lightspeed",
+    "neogen",
 
-		"toggleterm",
+    "toggleterm",
 
-		"gitsigns",
-		"blamer",
-		"octo",
+    "gitsigns",
+    "blamer",
+    "octo",
 
-		"neorg",
+    "neorg",
 
-		"lualine",
-	}
+    "lualine",
+  }
 
-	for _, name in ipairs(configs) do
-		require("plugins/" .. name).init(use)
-	end
+  for _, name in ipairs(configs) do
+    require("plugins/" .. name).init(use)
+  end
 
-	-- auto compile packer when config is changed
-	vim.cmd([[autocmd BufWritePost plugins.lua source <afile> | PackerCompile]])
+  -- auto compile packer when config is changed
+  vim.cmd([[autocmd BufWritePost plugins.lua source <afile> | PackerCompile]])
 end)

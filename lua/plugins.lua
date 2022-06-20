654328774a930464941667fcc19bfd43196979f6
inadeqtfuturs <alexj.christie@gmail.com>
2022-06-20 10:20:05 -0500

    feat(dashboard): update to latest dashboard

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,69 +2,51 @@
 local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({ "git", "clone", "https://github.com/wbthomason/packer.nvim", install_path })
-  vim.cmd("packadd packer.nvim")
+	fn.system({ "git", "clone", "https://github.com/wbthomason/packer.nvim", install_path })
+	vim.cmd("packadd packer.nvim")
 end
 
 return require("packer").startup(function(use)
-  use({ "wbthomason/packer.nvim" })
+	use({ "wbthomason/packer.nvim" })
 
-  local configs = {
-    -- theme
-    "substrata",
-    -- "nord",
-    -- "iceberg",
+	local configs = {
+		-- theme
+		"substrata",
 
-    -- which key
-    "which_key",
+		"which_key",
+		"dashboard",
+		"treesitter",
+		"telescope",
+		"nvimtree",
 
-    -- dashboard
-    "dashboard",
+		"cmp",
+		"lsp",
 
-    -- treesitter
-    "treesitter",
+		"barbar",
+		"indent_blankline",
+		"autopairs",
+		"autotag",
+		"surround",
+		"comments",
+		"cool",
+		"lightspeed",
+		"neogen",
 
-    -- finder
-    "telescope",
+		"toggleterm",
 
-    -- file tree
-    "nvimtree",
+		"gitsigns",
+		"blamer",
+		"octo",
 
-    -- autocomplete
-    "cmp",
+		"neorg",
 
-    -- lsp
-    "lsp",
+		"lualine",
+	}
 
-    -- ide
-    "barbar",
-    "indent_blankline",
-    "autopairs",
-    "autotag",
-    "surround",
-    "comments",
-    "cool",
-    "lightspeed",
-    "neogen",
+	for _, name in ipairs(configs) do
+		require("plugins/" .. name).init(use)
+	end
 
-    -- terminal
-    "toggleterm",
-
-    -- git
-    "gitsigns",
-    "blamer",
-    "octo",
-
-    "neorg",
-
-    -- lualine
-    "lualine",
-  }
-
-  for i, name in ipairs(configs) do
-    require("plugins/" .. name).init(use)
-  end
-
-  -- auto compile packer when config is changed
-  vim.cmd([[autocmd BufWritePost plugins.lua source <afile> | PackerCompile]])
+	-- auto compile packer when config is changed
+	vim.cmd([[autocmd BufWritePost plugins.lua source <afile> | PackerCompile]])
 end)

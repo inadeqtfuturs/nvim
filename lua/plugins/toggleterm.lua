3002f46078457c29f176ba4edb0ca737517774b5
inadeqtfuturs <alexj.christie@gmail.com>
2022-06-21 09:19:10 -0500

    fix(Terminal): rounded borders on LazyGit

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -11,7 +11,7 @@ function module.init(use)
 			})
 
 			local Terminal = require("toggleterm.terminal").Terminal
-			local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+			local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, float_opts = { border = "curved" } })
 
 			function _lazygit_toggle()
 				lazygit:toggle()
